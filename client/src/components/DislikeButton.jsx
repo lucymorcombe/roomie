1ad72430f7cf7febca89d0dcdaad0a0b10ee7c7b
Lucy Morcombe <lucymaymorcombe@gmail.com>
2025-07-09 15:36:38 +0100

    Began creating like and dislike buttons

---
+++ client/src/components/DislikeButton.jsx
@@ -0,0 +1,11 @@
+function DislikeButton() {
+    return (
+        <>
+        <div className="dislikeButton">
+            <p>{String.fromCharCode(215)}</p>
+        </div>
+        </>
+    )
+}
+
+export default DislikeButton
---
+++ client/src/components/LikeButton.jsx
@@ -0,0 +1,11 @@
+function LikeButton() {
+    return (
+        <>
+        <div className="likeButton">
+            <p>{String.fromCharCode(9829)}</p>
+        </div>
+        </>
+    )
+}
+
+export default LikeButton
